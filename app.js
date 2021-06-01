730f662d6f12483da6b3e9a56989b08554ce5112
juanDeGiorgi <juanmadegiorgi@gmail.com>
2021-06-01 17:49:27 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const views = path.resolve(__dirname,"views");
+
+const app = express();
+
+app.use(express.static("public"));
+
+
+app.get("/",(req,res) => res.sendFile(path.resolve(views,"index.html")));
+app.get("/babbage",(req,res) => res.sendFile(path.resolve(views,"babbage.html")));
+app.get("/berners-lee",(req,res) => res.sendFile(path.resolve(views,"berners-lee.html")));
+app.get("/clarke",(req,res) => res.sendFile(path.resolve(views,"clarke.html")));
+app.get("/hamilton",(req,res) => res.sendFile(path.resolve(views,"hamilton.html")));
+app.get("/hopper",(req,res) => res.sendFile(path.resolve(views,"hopper.html")));
+app.get("/lovelace",(req,res) => res.sendFile(path.resolve(views,"lovelace.html")));
+app.get("/turing",(req,res) => res.sendFile(path.resolve(views,"turing.html")));
+
+
+app.listen(3030,() => console.log("server on port 3030"));
