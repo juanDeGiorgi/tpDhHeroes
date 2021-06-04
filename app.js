f257998f2d8ad4476a336e44168276b5b9a3bcfe
juanDeGiorgi <juanmadegiorgi@gmail.com>
2021-06-04 15:49:00 -0300

    modificacion en los html

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const path = require("path");
 
-const views = path.resolve(__dirname,"views");
 
+//initializations
 const app = express();
 
+//midelwares
 app.use(express.static("public"));
 
 
-app.get("/",(req,res) => res.sendFile(path.resolve(views,"index.html")));
-app.get("/babbage",(req,res) => res.sendFile(path.resolve(views,"babbage.html")));
-app.get("/berners-lee",(req,res) => res.sendFile(path.resolve(views,"berners-lee.html")));
-app.get("/clarke",(req,res) => res.sendFile(path.resolve(views,"clarke.html")));
-app.get("/hamilton",(req,res) => res.sendFile(path.resolve(views,"hamilton.html")));
-app.get("/hopper",(req,res) => res.sendFile(path.resolve(views,"hopper.html")));
-app.get("/lovelace",(req,res) => res.sendFile(path.resolve(views,"lovelace.html")));
-app.get("/turing",(req,res) => res.sendFile(path.resolve(views,"turing.html")));
+//routing
+const views = path.resolve(__dirname,"views");
+
+app
+   .get("/",(req,res) => res.sendFile(path.resolve(views,"index.html")))
+   .get("/babbage",(req,res) => res.sendFile(path.resolve(views,"babbage.html")))
+   .get("/berners-lee",(req,res) => res.sendFile(path.resolve(views,"berners-lee.html")))
+   .get("/clarke",(req,res) => res.sendFile(path.resolve(views,"clarke.html")))
+   .get("/hamilton",(req,res) => res.sendFile(path.resolve(views,"hamilton.html")))
+   .get("/hopper",(req,res) => res.sendFile(path.resolve(views,"hopper.html")))
+   .get("/lovelace",(req,res) => res.sendFile(path.resolve(views,"lovelace.html")))
+   .get("/turing",(req,res) => res.sendFile(path.resolve(views,"turing.html")));
 
 
+//start server
 app.listen(3030,() => console.log("server on port 3030"));
